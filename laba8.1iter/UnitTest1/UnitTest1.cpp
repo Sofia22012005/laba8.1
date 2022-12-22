d97ac200015a20f0f8689d2958523c8156fc9ec3
Sofia22012005 <114824939+Sofia22012005@users.noreply.github.com>
2022-12-22 22:38:54 +0200

    Add files via upload

---
+++ laba8.1iter/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba8.1iter/laba8.1iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+    TEST_CLASS(UnitTest2)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            char text[99] = "hellowhile hello";
+            int actual = IsOrNot(text);
+            Assert::AreEqual(1, actual);
+        }
+    };
+}
---
+++ laba8.1rek/Unit Test 1/Unit Test 1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba8.1rek/laba8.1rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+    TEST_CLASS(UnitTest2)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            char text[99] = "hellowhile hello";
+            int actual = IsOrNot(text);
+            Assert::AreEqual(1, actual);
+        }
+    };
+}
